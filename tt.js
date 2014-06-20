30e639ded4fbc0938c82fe2b43a3169ee9e362b1
Matt Payne <Payne@MattPayne.org>
2014-06-19 19:39:56 -0500

    10,20,30 is green, yellow, red now

--- tt.js
+++ tt.js
@@ -13,8 +13,12 @@
 		        	},
 				interval: function() { 
 					console.log(this.count);
-					if (this.count > 10) {
-						 $('.clock').css('background-color', 'red')
+					if ((this.count > 10) && (this.count < 20)) {
+						 $('.clock').css('background-color', 'green');
+					} else if ((this.count > 20) && (this.count < 30)) {
+						 $('.clock').css('background-color', 'yellow');
+					} else if ((this.count > 30) && (this.count < 40)) {
+						 $('.clock').css('background-color', 'red');
 					}
 				}
 		        }
