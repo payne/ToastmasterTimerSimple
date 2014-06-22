09e0402d42ace188ec344d1955774673ce173a48
Matt Payne <Payne@MattPayne.org>
2014-06-22 11:38:21 -0500

    bg color now.  And Fixed bug.

--- tt.js
+++ tt.js
@@ -1,4 +1,3 @@
-
 		var clock;
 		
 		$(document).ready(function() {
@@ -14,14 +13,18 @@
 				interval: function() { 
 					console.log(this.count);
 					if ((this.count > 10) && (this.count < 20)) {
-						 $('.clock').css('background-color', 'green');
+						 $('.clock').css('background-color', 'GreenYellow');
+             $('body').css('background-color','GreenYellow');
 					} else if ((this.count > 20) && (this.count < 30)) {
 						 $('.clock').css('background-color', 'yellow');
+             $('body').css('background-color','yellow');
 					} else if ((this.count > 30) && (this.count < 40)) {
 						 $('.clock').css('background-color', 'red');
+             $('body').css('background-color','red');
 					}
 				}
-		        }
-		    });
+				}
+    }
+);
 
 		});
